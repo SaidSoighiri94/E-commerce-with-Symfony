e70efd3597866c58bbac9d27b129e7fc200016e9
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-09-25 00:45:32 +0200

    Mis en form des champs Full_name, email et password

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,11 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,7 +22,27 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('full_name',TextType::class,[
+                'label' => false,
+                'attr' => [
+                    'placeholder' =>'Enter Your Full Name',
+                    'class' => 'form-control'
+                ],
+                'row_attr' => [
+                    'class' => 'form-group mb-3'
+                ]
+            ])
+
+            ->add('email',EmailType::class,[
+                'label' => false,
+                'attr' => [
+                    'placeholder' =>'Enter Your email',
+                    'class' => 'form-control'
+                ],
+                'row_attr' => [
+                    'class' => 'form-group mb-3'
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                                 'mapped' => false,
                 'constraints' => [
@@ -26,8 +51,8 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                                // instead of being set onto the object directly,
+            ->add('plainPassword', RepeatedType::class, [
+                // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
@@ -40,8 +65,29 @@ class RegistrationFormType extends AbstractType
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
-                    ]),
+                    ])
                 ],
+                'first_options'=> [
+                        'label' => false,
+                        'attr' => [
+                            'placeholder' =>'Your Password Please....',
+                            'class' => 'form-control'
+                         ],
+                        'row_attr' => [
+                        'class' => 'form-group mb-3'
+                        ]
+                    ],
+                    'second_options'=> [
+                        'label' => false,
+                        'attr' => [
+                            'placeholder' =>'Confirm Your Password Please....',
+                            'class' => 'form-control'
+                         ],
+                        'row_attr' => [
+                        'class' => 'form-group mb-3'
+                        ]
+                    ]
+                
             ])
         ;
     }
