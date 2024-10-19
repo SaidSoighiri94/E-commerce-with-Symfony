80dbe08e2a0ffa781114d82f90ec6e300f768e3e
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-10-19 02:04:14 +0200

    Ajout des champs CategoryCrudController

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class CategoryCrudController extends AbstractCrudController
 {
@@ -15,14 +17,19 @@ class CategoryCrudController extends AbstractCrudController
         return Category::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name'),
+            TextField::new('slug')->setTargetFieldName('name'),
             TextEditorField::new('description'),
+            BooleanField::new('isMega'),
+            ImageField::new('imageUrl')
+            ->setUploadDir("public/assets/images/categories"),
+
         ];
     }
-    */
+    
 }
